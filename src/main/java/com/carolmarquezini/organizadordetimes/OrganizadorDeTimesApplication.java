40866d9a8ff484da2ad45dbb341ebed3cb131189
carolmarquezini <c.marquezini15@gmail.com>
2023-09-17 22:17:42 -0300

    first commit

---
+++ src/main/java/com/carolmarquezini/organizadordetimes/OrganizadorDeTimesApplication.java
@@ -0,0 +1,13 @@
+package com.carolmarquezini.organizadordetimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorDeTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorDeTimesApplication.class, args);
+	}
+
+}
